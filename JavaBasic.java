214672a29cb2a1f74d466b51d4bda95b82c7027c
RajiaRani <Rajiarani.syal@gmail.com>
2025-01-16 14:21:51 -0600

    added 16 jan 2025

---
+++ JavaBasic.java
@@ -0,0 +1,7 @@
+public class JavaBasic{
+    public static void main(String args[]){
+        System.out.print("Hello world");
+        System.out.println("Hello world");//using ln for the line space
+        System.out.print("Hello world");
+    }
+}
