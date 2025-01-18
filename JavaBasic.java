dac1f6039fa61a3fb6a39b2865014f83d6977273
RajiaRani <Rajiarani.syal@gmail.com>
2025-01-18 12:52:40 -0600

    added

--- JavaBasic.java
+++ JavaBasic.java
@@ -2,7 +2,19 @@ public class JavaBasic{
     public static void main(String args[]){
         System.out.print("Hello world");
         System.out.println("Hello world");//using ln for the line space
-        System.out.print("Hello world\n");
-        System.out.print("Hello world");
+        System.out.print("Hello world\n");//using \n for the line space
+        System.out.println("Hello world");
+        System.out.print("1,2,3,4\n");
+        System.out.println(" * * * *");
+        System.out.println(" * * * ");
+        System.out.println(" * * ");
+        System.out.println(" * ");
+
+        int x = 20;
+        int y = 30;
+        System.out.println(x+y);
+        String firstName = "Rajia";
+        String lastName = "Syal";
+        System.out.print(firstName +  lastName);
     }
 }
